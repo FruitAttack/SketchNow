567db6149ee15517c8d36f239b3a59492722703a
FruitAttack <Liammcoburn@gmail.com>
2025-03-27 16:22:39 -0600

    modified it to use a viewModel for the drawingView

--- app/src/main/java/com/example/drawingapp/DrawingViewModel.kt
+++ app/src/main/java/com/example/drawingapp/DrawingViewModel.kt
@@ -3,7 +3,6 @@ package com.example.drawingapp
 import android.app.Application
 import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.Paint
 import androidx.lifecycle.AndroidViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
